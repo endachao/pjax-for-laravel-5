eaa53dfd5fddfe8db856c022857bf134bbbf5bda
yuanchao <653069653@qq.com>
2015-10-28 16:31:49 +0800

    修改引入

--- src/views/pjax.blade.php
+++ src/views/pjax.blade.php
@@ -1,7 +1,7 @@
 
-<link rel="stylesheet" href="{{ asset('plugin/pjax/css/nprogress.css') }}">
-<script type="text/javascript" src="{{ asset('plugin/pjax/js/jquery.pjax.js') }}"></script>
-<script type="text/javascript" src="{{ asset('plugin/pjax/js/nprogress.js') }}"></script>
+<link rel="stylesheet" href="{{ asset('plugins/pjax/css/nprogress.css') }}">
+<script type="text/javascript" src="{{ asset('plugins/pjax/js/jquery.pjax.js') }}"></script>
+<script type="text/javascript" src="{{ asset('plugins/pjax/js/nprogress.js') }}"></script>
 <script>
     $(document).ready(function()
     {
